c5a553a4bf559bb26c7697e071b1ecf0d97474c3
Glenn Terjesen <glenn.terjesen@gmail.com>
2023-12-05 15:33:18 +0100

    fix: tokio time

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    # sleep 65 seconds
-    tokio::time::sleep(std::time::Duration::from_secs(65)).await;
+    tokio::time::sleep(tokio::time::Duration::from_millis(65000)).await;
+    println!("65000 ms have elapsed");
     let app = Router::new().route("/", get(handler));
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("Server listening on {}", addr);
